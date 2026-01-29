9f1ded91c1a49ff966fb1a9c5867a7ae141b1446
ThanhMinh1602 <ntminh16201@gmail.com>
2026-01-29 18:44:59 +0700

    done

--- lib/main.dart
+++ lib/main.dart
@@ -43,7 +43,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: 'Flutter Demo',
       defaultTransition: Transition.noTransition,
       debugShowCheckedModeBanner: false,
       theme: ThemeData(textTheme: GoogleFonts.paytoneOneTextTheme()),
